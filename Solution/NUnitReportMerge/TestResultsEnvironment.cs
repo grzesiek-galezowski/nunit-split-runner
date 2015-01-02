68ec60bd6d5d02086964f13b85ff97c018437371
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2015-01-02 21:55:45 +0100

    Removed offending gitignore entries
    these entries led to ignoring the TestResultsEnvironment.cs file

---
+++ Solution/NUnitReportMerge/TestResultsEnvironment.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml.Linq;
+
+namespace NUnitReportMerge
+{
+  public class TestResultsEnvironment
+  {
+    private readonly XElement _env;
+
+    public TestResultsEnvironment(XElement env)
+    {
+      _env = env;
+    }
+
+    public string UserDomain()
+    {
+      return _env.Attribute("user-domain").Value;
+    }
+
+    public string User()
+    {
+      return _env.Attribute("user").Value;
+    }
+
+    public string MachineName()
+    {
+      return _env.Attribute("machine-name").Value;
+    }
+
+    public string Cwd()
+    {
+      return _env.Attribute("cwd").Value;
+    }
+
+    public string PlatformVersion()
+    {
+      return _env.Attribute("platform").Value;
+    }
+
+    public string OsVersion()
+    {
+      return _env.Attribute("os-version").Value;
+    }
+
+    public string ClrVersion()
+    {
+      return _env.Attribute("clr-version").Value;
+    }
+
+    public string NUnitVersion()
+    {
+      return _env.Attribute("nunit-version").Value;
+    }
+
+    public static TestResultsEnvironment ExtractFrom(XDocument xDoc)
+    {
+      return new TestResultsEnvironment(xDoc.Element("test-results").Element("environment"));
+    }
+  }
+}
